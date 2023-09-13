75df5a9bb4f06609144e001d4ef75b48cb3949c1
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2023-09-13 21:29:58 +0700

    done

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,15 +5,57 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
     public Concert() {
     }
     public Concert(String title, String description) {
-        //TODO: set attributes
-
+     //TODO: set attributes
+        this.title = title;
+        this.description = description;
+     }
+    
+     public String getTitle() {
+     //TODO: return title
+     return title;
+     }
+    public void setTitle(String title){ 
+         this.title = title;
     }
- 
-    //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
+
+    public String getDescription() {
+    //TODO: return description
+    return description;
+    }
+    public void setDescriptione(String description){
+    this.description = description;
+    }
+
+
+    //TODO: add getters and setters
+    public void setId(int id) {
+        this.id = id;
+     }
+    public int getId() {
+        return id;
+     }
+
+    public Date getDate() {
+        return date;
+     }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPerformer() {
+        return performer;
+}
+    public void setPerformer(String performer) {
+        this.performer = performer;
+}
 
 }
